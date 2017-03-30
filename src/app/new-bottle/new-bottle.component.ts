774695d6d31e86d879af30b9825d75662d74431f
Michaela Davis and Sarah Leahy <sarah@>
2017-03-30 11:31:37 -0700

    show/hide employee portal

--- src/app/new-bottle/new-bottle.component.ts
+++ src/app/new-bottle/new-bottle.component.ts
@@ -10,10 +10,15 @@ export class NewBottleComponent implements OnInit {
   @Input() childSelectedBottle: Bottle;
   @Output() newBottleSender = new EventEmitter();
 
+  showPortal: boolean = false;
+
   submitForm(brand: string,  pricePerBottle: number,  pricePerShot: number,  proof: number,  distillery: string,  shots: number,  flavor: string,  color: string,  alcoholType: string) {
       var newBottleToAdd: Bottle = new Bottle(brand,  pricePerBottle,  pricePerShot,  proof,  distillery,  shots,  flavor,  color,  alcoholType);
       this.newBottleSender.emit(newBottleToAdd);
+  }
 
+  showEmployeePortal() {
+    this.showPortal = true;
   }
 
   constructor() { }
